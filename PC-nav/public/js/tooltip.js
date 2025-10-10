79558b68a9c523196b3dca24addb7862baa59eff
Migle <dubauskaitemigle@gmail.com>
2025-10-10 20:17:33 +0200

    ejercicio de clase

--- PC-nav/public/js/tooltip.js
+++ PC-nav/public/js/tooltip.js
@@ -1,20 +1,36 @@
-class Tooltip{
-    constructor({fondo='black', texto='white'}){
-        this.fondo = fondo;
-        this.texto = texto;
-        this.render();
-    }
+class Tooltip {
+  constructor({ fondo = "black", texto = "white" }) {
+    this.fondo = fondo;
+    this.texto = texto;
+    this.render();
+  }
 
-    render(){
-        /* ponernos encima de  icono floppy disk y que nos salga mensaje guardar cambios */
-        const etiqueta = document.createElement('div');
-        const iconos = document.getElementsByClassName('icono');
-        
-        for(const icono of iconos){
-            icono.addEventListener('mouseover', function(){
-                etiqueta.innerText = icono.dataset.titulo;
-            })
-        }
+  render() {
+    /* ponernos encima de  icono floppy disk y que nos salga mensaje guardar cambios */
+    const iconos = document.getElementsByClassName("icono");
 
+    for (const icono of iconos) {
+      icono.addEventListener("mouseover", (e) => {
+        icono.style.position = "relative";
+        const etiqueta = document.createElement("div");
+        etiqueta.innerText = icono.dataset.titulo;
+
+        etiqueta.style.position = "absolute";
+        etiqueta.style.bottom = "100%"; // encima del icono
+        etiqueta.style.background = this.fondo;
+        etiqueta.style.color = this.texto;
+        etiqueta.style.borderRadius = "5px";
+        etiqueta.style.whiteSpace = "nowrap";
+        etiqueta.style.zIndex = "1000";
+
+        icono.append(etiqueta);
+
+        icono.addEventListener("mouseout", () => {
+          etiqueta.remove();
+        });
+      });
     }
+  }
 }
+
+new Tooltip({ fondo: "grey", texto: "purple" });
